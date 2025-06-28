50284040510b0d90af43cabfb336aee7577f01c1
Juan David Moreno Mañunga <149904966+JounBrown@users.noreply.github.com>
2025-06-28 10:22:15 -0500

    Frontend Responsivo - Modificaciones CSS

--- Kredilibranza FrontEnd/Kredilibranza/src/layouts/MainLayout.jsx
+++ Kredilibranza FrontEnd/Kredilibranza/src/layouts/MainLayout.jsx
@@ -6,7 +6,12 @@ function MainLayout({ children }) {
   return (
     <div className="AppLayout">
       <NavBar />
-      <main style={{ paddingTop: '10vh' }}>
+      <main style={{ 
+        paddingTop: '10vh',
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column'
+      }}>
         {children}
       </main>
       <Footer />
